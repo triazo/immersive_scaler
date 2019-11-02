329e7e427acd782f4ff4d6444915385445b22f21
Adam Freeman <adam@triazo.net>
2019-11-01 23:47:45 -0400

    fix A-pose, start finger spread
    afaict finger spread is rotating based on local coordinates of the bone instead of global coordinates. It's close enough to be usable in some cases but not what I would call finished

--- plugin/main.py
+++ plugin/main.py
@@ -10,8 +10,6 @@ bl_info = {
 def get_objects():
     return bpy.context.scene.objects
 
-
-
 def get_armature(armature_name=None):
     if not armature_name:
         armature_name = bpy.context.scene.armature
@@ -38,11 +36,9 @@ def measure_wingspan(obj):
             bone_gap = (pose_bones[l[i]].head - pose_bones[l[i+1]].tail).length
             bone_length = pose_bones[l[i]].length
             length += bone_gap + bone_length
-    print(length)
 
     left_hand = pose_bones['Left wrist']
     right_hand = pose_bones['Right wrist']
-    print((left_hand.head - right_hand.head).length)
 
 def get_lowest_point():
     body = bpy.data.objects['Body']
@@ -65,35 +61,58 @@ def get_highest_point():
 
     return(highest_vertex.co[2])
 
-
 def get_view_y(obj):
-    # TODO: go to T-pose and reset
+    # VRC uses the distance between the head bone and right hand in
+    # t-pose as the basis for world scale. Enforce t-pose locally to
+    # grab this number
+    bpy.ops.object.mode_set(mode='POSE', toggle = False)
 
     # Gets the in-vrchat virtual height that the view will be at,
     # relative to your actual floor.
-    rhandpos = obj.pose.bones['Right wrist'].head
-    headpos = obj.pose.bones['Head'].head
 
     # Magic that somebody posted in discord. I'm going to just assume
-    # these constants are correct.
-    view_y = ((headpos - rhandpos).length / .4537) + .005
-    print("View coord is %f"%view_y)
+    # these constants are correct. Testing shows it's at least pretty
+    # darn close
+    view_y = (head_to_hand(obj) / .4537) + .005
+    bpy.ops.object.mode_set(mode='POSE', toggle = True)
 
     return view_y
+
+def head_to_hand(obj):
+    # Since arms might not be flat, add the length of the arm to the x
+    # coordinate of the shoulder
+    headpos = obj.pose.bones['Head'].head
+    shoulder = obj.pose.bones['Right arm'].head
+    arm_length = (obj.pose.bones['Right arm'].head - obj.pose.bones['Right wrist'].head).length
+    arm_length = (obj.pose.bones['Right arm'].length + obj.pose.bones['Right elbow'].length)
+    t_hand_pos = mathutils.Vector((shoulder[0] - arm_length, shoulder[1], shoulder[2]))
+    print(t_hand_pos)
+    bpy.context.scene.cursor.location = t_hand_pos
+    return (headpos - t_hand_pos).length
 
 def calculate_arm_rescaling(obj, head_arm_change):
     # Calculates the percent change in arm length needed to create a
     # given change in head-hand length.
 
-    # TODO: go to T-pose to measure and reset at the end
+    bpy.ops.object.mode_set(mode='POSE', toggle = False)
 
     rhandpos = obj.pose.bones['Right wrist'].head
     rarmpos = obj.pose.bones['Right arm'].head
     headpos = obj.pose.bones['Head'].head
 
-    total_length = (headpos - rhandpos).length
+    # Reset t-pose to whatever it was before since we have the data we
+    # need
+    bpy.ops.object.mode_set(mode='POSE', toggle = True)
+
+    total_length = head_to_hand(obj)
     arm_length = (rarmpos - rhandpos).length
-    neck_length = abs((headpos[2] - rhandpos[2]))
+    neck_length = abs((headpos[2] - rarmpos[2]))
+
+    # Sanity check - compare the difference between head_to_hand and manual
+    # print("")
+    # print("-------head_to_hand: %f" %total_length)
+    # print("-------manual, assuming t-pose: %f" %(headpos - rhandpos).length)
+    # print("")
 
     # Also derived using sympy. See below.
     shoulder_length = math.sqrt((total_length - neck_length) * (total_length + neck_length)) - arm_length
@@ -137,17 +156,18 @@ def get_leg_length(obj):
 
 def scale_to_floor(arm_to_legs):
     obj = bpy.data.objects['Armature']
-    # bo_context(bpy, bpy.ops.object.mode_set, mode='POSE', toggle = False)
-    bpy.ops.cats_manual.start_pose_mode()
     arm = get_armature()
 
     view_y = get_view_y(obj)
     eye_y = get_eye_height(obj)
 
-    # Compensate for the difference in in-game view and actual view with leg height
+    # TODO: add an option for people who *want* their legs below the floor.
+    #
+    # weirdos
     rescale_ratio = eye_y / view_y
     leg_height_portion = get_leg_length(obj) / eye_y
 
+    # Enforces: rescale_leg_ratio * rescale_arm_ratio = rescale_ratio
     rescale_leg_ratio = rescale_ratio ** arm_to_legs
     rescale_arm_ratio = rescale_ratio ** (1-arm_to_legs)
 
@@ -158,13 +178,15 @@ def scale_to_floor(arm_to_legs):
     print("Scaling legs by a factor of %f" % leg_scale_ratio)
     print("Scaling arms by a factor of %f" % arm_scale_ratio)
 
-    # TODO: play aronud with scaling in the other directions as
-    # well. How should this be exposed?
+    bpy.ops.cats_manual.start_pose_mode()
+
     for leg in ["Left leg", "Right leg"]:
         obj.pose.bones[leg].scale = (1, leg_scale_ratio, 1)
-
     for arm in ["Left arm", "Right arm"]:
         obj.pose.bones[arm].scale = (1, arm_scale_ratio, 1)
+    for hand in ["Left wrist", "Right wrist"]:
+        obj.pose.bones[hand].scale = (1, 1 / arm_scale_ratio, 1)
+
 
     try:
         bpy.ops.cats_manual.pose_to_rest()
@@ -212,12 +234,43 @@ def scale_to_height(new_height):
     scale_ratio = new_height / old_height
     bpy.context.scene.cursor.location = (0, 0, 0)
     bpy.ops.transform.resize( value = (scale_ratio, scale_ratio, scale_ratio) )
+
+    # TODO: This apply only seems to affect the top level object -
+    # points in the mesh don't get scaled correctly and running this
+    # prevents running the script again. Maybe I can just call out to
+    # CATS again?
     bpy.ops.object.transform_apply(location = True, scale = True,  rotation = True)
 
 def rescale_main(new_height, arm_to_legs):
     scale_to_floor(arm_to_legs)
     move_to_floor()
     scale_to_height(new_height)
+
+
+def point_bone(bone, point):
+    v1 = (bone.tail - bone.head).normalized()
+    v2 = (bone.head - point).normalized()
+    vg = bone.id_data.matrix_world.to_quaternion()
+    a = v2.cross(v1)
+
+    print("Pose bone rotation: %s" % str(vg))
+
+    w = 1 + v1.dot(v2)
+    print(v1, v2, w)
+
+    global_quat = mathutils.Quaternion((w,a[0],a[1],a[2])).normalized().inverted()
+    global_quat.rotate(vg)
+
+    bone.rotation_quaternion = global_quat
+
+def spread_fingers():
+    obj = bpy.data.objects['Armature']
+    bpy.ops.cats_manual.start_pose_mode()
+    for hand_name in ['Right wrist', 'Left wrist']:
+        hand = obj.pose.bones[hand_name]
+        for finger in hand.children:
+            point_bone(finger, hand.head)
+    bpy.ops.cats_manual.pose_to_rest()
 
 
 class ArmatureLegsToFloor(bpy.types.Operator):
@@ -269,12 +322,37 @@ class ArmatureRescale(bpy.types.Operator):
 
         return {'FINISHED'}
 
+class ArmatureForceT(bpy.types.Operator):
+    """Forces an armature into t-pose"""
+    bl_idname = "armature.tpose"
+    bl_label = "Force T Pose"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        bpy.ops.object.mode_set(mode='POSE', toggle = False)
+        obj = get_armature()
+        print(head_to_hand(obj))
+
+        return {'FINISHED'}
+
+class ArmatureSpreadFingers(bpy.types.Operator):
+    """Spreads the fingers on a humanoiod avatar"""
+    bl_idname = "armature.spreadfingers"
+    bl_label = "Spread Fingers"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        spread_fingers()
+        return {'FINISHED'}
+
 
 def register():
     bpy.utils.register_class(ArmatureLegsToFloor)
     bpy.utils.register_class(ArmatureMoveToFloor)
     bpy.utils.register_class(ArmatureScaleToHeight)
     bpy.utils.register_class(ArmatureRescale)
+    bpy.utils.register_class(ArmatureForceT)
+    bpy.utils.register_class(ArmatureSpreadFingers)
     print("Registering Armature tuning add-on")
 
 def unregister():
@@ -283,6 +361,8 @@ def unregister():
     bpy.utils.unregister_class(ArmatureMoveToFloor)
     bpy.utils.unregister_class(ArmatureScaleToHeight)
     bpy.utils.unregister_class(ArmatureRescale)
+    bpy.utils.unregister_class(ArmatureForceT)
+    bpy.utils.unregister_class(ArmatureSpreadFingers)
     print("Unregistering Armature tuning add-on")
 
 if __name__ == "__main__":
